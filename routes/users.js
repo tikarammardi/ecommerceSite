442bf77b8ab6e8f512ffb7358721f88ca62c1afb
tikarammardi <tikarammardi007@gmail.com>
2018-11-09 00:35:00 +0530

    Registration functionality working properly  insertion duplicate email yet to be done

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
+let bcrypt = require('bcryptjs');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+const connection = require('../config/database');
+
 
 //Login Page - GET REQUEST
 router.get('/login', (req, res) => {
     res.render('login');
-})
+});
 
 
 //Register Page - GET REQUEST
 router.get('/register', (req, res) => {
     res.render('register');
 });
+
+//logout
 
 router.get('/logout', (req, res) => {
     req.logout();
@@ -19,6 +26,115 @@ router.get('/logout', (req, res) => {
 
 });
 
+//Register Page - POST REQUEST
+router.post('/register', (req, res) => {
 
+    // Get Form Values
+
+    let person = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+        mobile: req.body.mobile,
+        gender: req.body.gender
+    }
+    //Validation
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').isEmail();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Password Do not match').equals(req.body.password);
+    req.checkBody('address', 'Address field is required').notEmpty();
+    req.checkBody('city', 'City field is required').notEmpty();
+    req.checkBody('zip', 'Zip Code Required').notEmpty();
+    req.checkBody('mobile', 'Mobile No. required').notEmpty();
+
+
+    //check for errors
+    let errors = req.validationErrors();
+    if (errors) {
+        console.log("Form has errors...........")
+        res.render('register', {
+            errors: errors,
+            name: person.name,
+            email: person.email,
+            address: person.address,
+            city: person.city,
+            zip: person.zip,
+            mobile: person.mobile
+        })
+    } else {
+
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(person.password, salt, (err, hash) => {
+                person.password = hash;
+
+                let q = 'INSERT INTO customers SET ?';
+                connection.query(q, person, (err, results) => {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        console.log("User added .........")
+                        //Our success message
+                        req.flash('success', 'Your are registered and can now log in');
+                        console.log(results);
+                        //REdiredt after user registeration
+                        res.location('/');
+                        res.redirect('/');
+                    }
+                });
+            });
+        });
+
+    }
+
+});
+
+//Sign in by grabbing username and password from the sign in page AND
+//finding our  user by the username from the database and then
+//Matching it's enctypted password with entered password
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        db.users.findOne({
+            username: username
+        }, (err, user) => {
+            if (err) return done(err);
+
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect Username'
+                });
+            }
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) return done(err);
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, {
+                        message: 'Incorrect Password'
+                    });
+                }
+            });
+
+        });
+    }
+));
+
+//Login-POST
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: 'Invalid Username or Password'
+    }),
+    (req, res) => {
+        console.log('Authentication Successful');
+        res.redirect('/');
+    });
 
 module.exports = router;
